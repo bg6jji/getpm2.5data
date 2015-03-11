2ccb5abbf9675ab334ed0203eaa09ea6c0162413
bg6jji <bg6jji@gmail.com>
2015-03-11 17:41:07 +0800

    Update getpm25.lua
    增加DNS失效判定
    去掉了GET 的城市代码

--- getpm25.lua
+++ getpm25.lua
@@ -11,25 +11,23 @@
     --gpio.write(0,gpio.LOW)
     gpio.write(2,gpio.LOW)
 
-    delay = tmr.delay
-
     pwm.setup(8, 500, 512)  pwm.setup(7, 500, 512)  pwm.setup(6, 500, 512) 
-    pwm.start(8)  pwm.start(7)  pwm.start(6)  
+    pwm.start(8)            pwm.start(7)            pwm.start(6)  
 
     function led(r,g,b) 
     pwm. setduty(6,g)  pwm. setduty(7,b)  pwm. setduty(8,r)  end 
     led(0, 0, 0) -- led关闭显示
 
     function ledflash(flashtime)
-      gpio.write(5,gpio.HIGH) delay(flashtime*1000) gpio.write(5,gpio.LOW) delay(flashtime*1000)
-      gpio.write(5,gpio.HIGH) delay(flashtime*1000) gpio.write(5,gpio.LOW) delay(flashtime*1000)
+      gpio.write(5,gpio.HIGH) tmr.delay(flashtime*1000) gpio.write(5,gpio.LOW) tmr.delay(flashtime*1000)
+      gpio.write(5,gpio.HIGH) tmr.delay(flashtime*1000) gpio.write(5,gpio.LOW) tmr.delay(flashtime*1000)
     end
 
     print("开始内存:"..node.heap())
     tmr.alarm(0, 30000, 1, function()   --请将抓取时间改大一些，减少服务器负载。
      --创建一个TCP连接
      socket=net.createConnection(net.TCP, 0)
-     --socket:dns(Server, function(conn,ServerIP) ServerIP = ip end)
+     socket:dns(Server, function(conn,ip) if ip == nil then print("DNS Fail.") node.restart() end end)
      --开始连接服务器
      socket:connect(80, Server)
      socket:on("connection", function(sck) 
@@ -41,9 +39,10 @@
      --HTTP请求头定义
 
      local ApiKey = "xxxxxx"     --这个是个共用APIKEY，请换成自己的，没有申请的，请到www.pm25.in申请。API说明请看http://www.pm25.in/api_doc
-     local city = "xxxx"                  --获取城市
+     --local city = "xxxx"                  --获取城市
      local station_code = "xxxx"              --采样点编号
-     socket:send("GET /api/querys/aqis_by_station.json?city="..city.."&station_code="..station_code.."&token="..ApiKey.." HTTP/1.1\r\n" ..    --这里只是获取某个监测点的数据
+     socket:send("GET /api/querys/aqis_by_station.json?station_code="..station_code..  --"&city="..city..
+            "&token="..ApiKey.." HTTP/1.1\r\n" ..    --这里只是获取某个监测点的数据
             "Host: www.pm25.in\r\n" ..
             "Accept: */*" ..
             "User-Agent: Nodemcu\r\n\r\n")
@@ -65,17 +64,17 @@
           local time1=string.sub(Aqijson,(Tm+13),(Tm+32))  
           tm = nil  Aqijson = nil
           print("综合:"..aqidata.."  PM10:"..pm10.."  PM2.5:"..pm25)
-          print("监测点:"..pn.."  监测时间:"..time1)
+          print("监测点:"..pn.."  采样时间:"..time1)
           print(node.heap())
           aqidata = tonumber(aqidata) pm10 = tonumber(pm10) pm25 = tonumber(pm25)
           led(aqidata, pm10, pm25)    --LED按数值点亮，越亮污染越重
-          if aqidata >= 200 then          ledflash(2000)
-            elseif aqidata >= 150 then    ledflash(1000) 
-               elseif aqidata >= 100 then ledflash(500) end
+          if aqidata >= 200 then          ledflash(1500)
+            elseif aqidata >= 150 then    ledflash(600) 
+               elseif aqidata >= 100 then ledflash(200) end
           
           
       end
       socket:close()
       end)
-     --socket:on("disconnection", function(conn, pl) print("disconnection from:"..Server) socket:close() end) 
+      --socket:on("disconnection", function(conn, pl) print("disconnection from:"..Server) socket:close() node.restart() end) 
    end)
