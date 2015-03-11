cf7eafb37f0836c8b52412135e4146dfacd70397
bg6jji <bg6jji@gmail.com>
2015-03-11 15:22:27 +0800

    Update getpm25.lua
    add pwm led.
    增加pwm led指示AQI/PM2.5参数情况。

--- getpm25.lua
+++ getpm25.lua
@@ -1,5 +1,12 @@
 
     Server = "www.pm25.in"
+    pwm.setup(8, 500, 512)  pwm.setup(7, 500, 512)  pwm.setup(6, 500, 512) 
+    pwm.start(8)  pwm.start(7)  pwm.start(6)  
+
+    function led(r,g,b) 
+    pwm. setduty(6,g)  pwm. setduty(7,b)  pwm. setduty(8,r)  end 
+    led(0, 0, 0) -- led显示红色  led(0, 0, 512) -- led显示蓝色
+
     print("开始内存:"..node.heap())
     tmr.alarm(0, 30000, 1, function()   --请将抓取时间改大一些，减少服务器负载。
      --创建一个TCP连接
@@ -14,9 +21,9 @@
      function toget()
      --HTTP请求头定义
 
-     local ApiKey = "xxxxxxxxx"     --这个是个共用APIKEY，请换成自己的，没有申请的，请到www.pm25.in申请。API说明请看http://www.pm25.in/api_doc
-     local city = "xxxxx"                  --获取城市
-     local station_code = "xxxxx"              --采样点编号
+     local ApiKey = "5j1znBVAsnSf5xQyNQyq"     --这个是个共用APIKEY，请换成自己的，没有申请的，请到www.pm25.in申请。API说明请看http://www.pm25.in/api_doc
+     local city = "zhengzhou"                  --获取城市
+     local station_code = "1321A"              --采样点编号
      socket:send("GET /api/querys/aqis_by_station.json?city="..city.."&station_code="..station_code.."&token="..ApiKey.." HTTP/1.1\r\n" ..    --这里只是获取某个监测点的数据
             "Host: www.pm25.in\r\n" ..
             "Accept: */*" ..
@@ -37,6 +44,9 @@
           local Tm=string.find(Aqijson,"time_point")
           local time1=string.sub(Aqijson,(Tm+13),(Tm+32))
           print("综合:"..aqidata.."PM2.5:"..pm25.."监测点:"..pn.."监测时间:"..time1)
+          
+          led(tonumber(aqidata), tonumber(pm25), 0)
+
           print(node.heap())
       end
       socket:close()
